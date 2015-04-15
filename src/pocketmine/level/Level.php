0773a9cdf466ce276afa0f9046559ca90b084889
Blubbll <Blubbll@users.noreply.github.com>
2015-04-15 18:36:44 +0200

    Updated Level.php to not crash on null
    Fixed a bug that crashed the Server when a Plugin calls a Level name which is null.
    Returns the level world instead which cannot be unload (due to this might change or be instable, this can only be seen as a Hack/way around the problem).
    Hope this helps others aswell.

--- src/pocketmine/level/Level.php
+++ src/pocketmine/level/Level.php
@@ -2277,11 +2277,18 @@ class Level implements ChunkManager, Metadatable{
 
 	/**
 	 * Returns the Level name
-	 *
+	 * If null, world gets returned. This prevents World Managing plugins from Crashing the Server on null
 	 * @return string
 	 */
-	public function getName(){
+	
+		if ($this->provider->getName() === null) {
+		return world;
+		}
+		
+		else 
+
 		return $this->provider->getName();
+	
 	}
 
 	/**
